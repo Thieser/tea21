0cc9b750d7660119fae7a66d8ed2fbce6e07f8c5
Thieser <149794167+Thieser@users.noreply.github.com>
2023-11-03 15:09:42 +0100

    TEst

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -9,12 +9,23 @@
 
 auto main(int argc, char **argv) -> int
 {
+    //.data segment
+    int data =4711;
+    fmt::ptr(&data);    
+
+    // .roaddata segment 
+    int roaddata = 455;
+
+    fmt::ptr(&roaddata);    
+
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    int foo;
-    fmt::print("Hello, {}!\n", "hello");
+    int foo = 0;
+    fmt::ptr(&foo);    
+    
+    fmt::print("Value of variable foo {} address of variable foo{}",foo,fmt::ptr(&foo));
     return 0; /* exit gracefully*/
 }
