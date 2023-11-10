0ac349a6169e6a886a117a91dd300b9f7d5216cc
Thieser <149794167+Thieser@users.noreply.github.com>
2023-11-10 13:58:04 +0100

    Test

--- exercise-003/main.cpp
+++ exercise-003/main.cpp
@@ -13,7 +13,7 @@ auto main(int argc, char **argv) -> int
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
-     * More info at https://fmt.dev/latest/api.html
+     * More info at https://fmt.dev/latest/api.html test
      */
     fmt::print("Hello, {}!\n", "World");
 
